e318fa2f202f6064b652ef9e42c85f6b186c2e94
FildaStupka <stupkafilip@gmail.com>
2021-11-16 21:28:23 +0100

    The snake is moving
    Interval set, startBtn = emergency clearInterval btn

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const grid = document.getElementById("grid")
 const width = 10
 let squares = []
 let currentSnake = [2,1,0]
+let direction = 1
 
 function createGrid() {
     for (let i = 0; i < width*width; i++) {
@@ -16,3 +17,15 @@ function createGrid() {
 createGrid()
 
 currentSnake.forEach (index => squares[index].classList.add("snake"))
+
+function move() {
+    const tail = currentSnake.pop()
+    squares[tail].classList.remove("snake")
+    currentSnake.unshift(currentSnake[0] + direction)
+    squares[currentSnake[0]].classList.add("snake")
+}
+
+let interval = setInterval(move, 1000)
+
+//delete
+startBtn.addEventListener("click", function() {clearInterval(interval)})
