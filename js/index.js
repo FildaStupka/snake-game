44a73e8cf870a159c8839d9aa8e8fb787c921c13
FildaStupka <stupkafilip@gmail.com>
2021-11-17 10:03:13 +0100

    Apples, growth, score and stop game refactor

--- js/index.js
+++ js/index.js
@@ -8,12 +8,13 @@ let squares = []
 let currentSnake = [6,5,4,3,2,1,0]
 let direction = 1
 let interval = 0
+let appleLocation
+let score = 0
 
 function createGrid() {
     for (let i = 0; i < width*width; i++) {
         const square = document.createElement("div")
         square.classList.add("square")
-        square.textContent = i
         grid.appendChild(square)
         squares.push(square)
     }
@@ -23,6 +24,7 @@ createGrid()
 currentSnake.forEach (index => squares[index].classList.add("snake"))
 
 function startGame() {
+    generateApple()
     interval = setInterval(move, 1000)
 }
 
@@ -31,40 +33,65 @@ function restartGame() {
     currentSnake = [6,5,4,3,2,1,0]
     direction = 1
     currentSnake.forEach (index => squares[index].classList.add("snake"))
+    score = 0
+    displayScore.textContent = score
 }
 
+function stopGame() {
+    clearInterval(interval)
+    console.log("You lost!")
+}
+
+//movement, growth and endings
 function move() {
+    //endings
     if ((currentSnake[0] % 10 === 9 && direction === 1) ||
         (currentSnake[0] + direction >= 100) ||
         (currentSnake[0] % 10 === 0 && direction === -1) ||
         (currentSnake[0] + direction < 0) ||
         squares[currentSnake[0] + direction].classList.contains("snake")) 
-        return clearInterval(interval)
+        return stopGame()
 
+    //movement
     const tail = currentSnake.pop()
     squares[tail].classList.remove("snake")
     currentSnake.unshift(currentSnake[0] + direction)
     squares[currentSnake[0]].classList.add("snake")
-}
 
-function control(e) {
-    if (e.keyCode === 39 || e.key === "ArrowRight") {
-        direction = 1
-        console.log("right pressed")
-    } else if (e.keyCode === 40 || e.key === "ArrowDown") {
-        direction = 10
-        console.log("down pressed")
-    } else if (e.keyCode === 37 || e.key === "ArrowLeft") {
-        direction = -1
-        console.log("left pressed")
-    } else if (e.keyCode === 38 || e.key === "ArrowUp") {
-        direction = -10
-        console.log("up pressed")
+    //growth
+    if (squares[currentSnake[0]].classList.contains("apple")) {
+        squares[currentSnake[0]].classList.remove("apple")
+        currentSnake.push(tail)
+        squares[tail].classList.add("snake")
+        generateApple()
+        score += 1
+        displayScore.textContent = score
     }
 }
 
+//constrols
+function control(e) {
+    if (e.keyCode === 39 || e.key === "ArrowRight") {
+        direction = 1
+    } else if (e.keyCode === 40 || e.key === "ArrowDown") {
+        direction = 10
+    } else if (e.keyCode === 37 || e.key === "ArrowLeft") {
+        direction = -1
+    } else if (e.keyCode === 38 || e.key === "ArrowUp") {
+        direction = -10
+    }
+}
+
+//apples
+function generateApple() {
+    do {
+        appleLocation = Math.floor(Math.random()*100)
+    } while (squares[appleLocation].classList.contains("snake"))
+    squares[appleLocation].classList.add("apple")
+}
+
+//listeners
 document.addEventListener("keyup", control)
-//delete
 startBtn.addEventListener("click", startGame)
 restartBtn.addEventListener("click", restartGame)
-stopBtn.addEventListener("click", function() {clearInterval(interval)})
+stopBtn.addEventListener("click", stopGame)
